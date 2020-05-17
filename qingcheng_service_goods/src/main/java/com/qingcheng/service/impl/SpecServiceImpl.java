80be450fc479517f68038e6a30a59352d54ba518
GUANX <CST123456>
2020-05-17 17:20:42 +0800

    商品规格参数管理

--- qingcheng_service_goods/src/main/java/com/qingcheng/service/impl/SpecServiceImpl.java
+++ qingcheng_service_goods/src/main/java/com/qingcheng/service/impl/SpecServiceImpl.java
@@ -2,33 +2,35 @@ package com.qingcheng.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-import com.qingcheng.dao.ParaMapper;
+import com.qingcheng.dao.SpecMapper;
 import com.qingcheng.dao.TemplateMapper;
 import com.qingcheng.entity.PageResult;
-import com.qingcheng.pojo.goods.Para;
+import com.qingcheng.pojo.goods.Spec;
 import com.qingcheng.pojo.goods.Template;
-import com.qingcheng.service.goods.ParaService;
+import com.qingcheng.service.goods.SpecService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 import java.util.Map;
 
-@Service(interfaceClass = ParaService.class)
-public class ParaServiceImpl implements ParaService {
+@Service(interfaceClass = SpecService.class)
+public class SpecServiceImpl implements SpecService {
 
     @Autowired
-    private ParaMapper paraMapper;
+    private SpecMapper specMapper;
 
     @Autowired
     private TemplateMapper templateMapper;
+
 
     /**
      * 返回全部记录
      * @return
      */
-    public List<Para> findAll() {
-        return paraMapper.selectAll();
+    public List<Spec> findAll() {
+        return specMapper.selectAll();
     }
 
     /**
@@ -37,10 +39,10 @@ public class ParaServiceImpl implements ParaService {
      * @param size 每页记录数
      * @return 分页结果
      */
-    public PageResult<Para> findPage(int page, int size) {
+    public PageResult<Spec> findPage(int page, int size) {
         PageHelper.startPage(page,size);
-        Page<Para> paras = (Page<Para>) paraMapper.selectAll();
-        return new PageResult<Para>(paras.getTotal(),paras.getResult());
+        Page<Spec> specs = (Page<Spec>) specMapper.selectAll();
+        return new PageResult<Spec>(specs.getTotal(),specs.getResult());
     }
 
     /**
@@ -48,9 +50,9 @@ public class ParaServiceImpl implements ParaService {
      * @param searchMap 查询条件
      * @return
      */
-    public List<Para> findList(Map<String, Object> searchMap) {
+    public List<Spec> findList(Map<String, Object> searchMap) {
         Example example = createExample(searchMap);
-        return paraMapper.selectByExample(example);
+        return specMapper.selectByExample(example);
     }
 
     /**
@@ -60,11 +62,11 @@ public class ParaServiceImpl implements ParaService {
      * @param size
      * @return
      */
-    public PageResult<Para> findPage(Map<String, Object> searchMap, int page, int size) {
+    public PageResult<Spec> findPage(Map<String, Object> searchMap, int page, int size) {
         PageHelper.startPage(page,size);
         Example example = createExample(searchMap);
-        Page<Para> paras = (Page<Para>) paraMapper.selectByExample(example);
-        return new PageResult<Para>(paras.getTotal(),paras.getResult());
+        Page<Spec> specs = (Page<Spec>) specMapper.selectByExample(example);
+        return new PageResult<Spec>(specs.getTotal(),specs.getResult());
     }
 
     /**
@@ -72,48 +74,46 @@ public class ParaServiceImpl implements ParaService {
      * @param id
      * @return
      */
-    public Para findById(Integer id) {
-        return paraMapper.selectByPrimaryKey(id);
+    public Spec findById(Integer id) {
+        return specMapper.selectByPrimaryKey(id);
     }
 
     /**
      * 新增
-     * @param para
+     * @param spec
      */
-    public void add(Para para) {
-        //添加数据到参数表
-        paraMapper.insert(para);
-        
-        //更新模板表中参数规格数
-        Integer templateId = para.getTemplateId();
+    @Transactional
+    public void add(Spec spec) {
+        specMapper.insert(spec);
+        //将模板中的规格数量+1
+        Integer templateId = spec.getTemplateId();
         Template template = templateMapper.selectByPrimaryKey(templateId);
-        template.setParaNum(template.getParaNum() + 1);
+        Integer specNum = template.getSpecNum();
+        template.setSpecNum(specNum + 1);
         templateMapper.updateByPrimaryKey(template);
     }
 
     /**
      * 修改
-     * @param para
+     * @param spec
      */
-    public void update(Para para) {
-        paraMapper.updateByPrimaryKeySelective(para);
+    public void update(Spec spec) {
+        specMapper.updateByPrimaryKeySelective(spec);
     }
 
     /**
      *  删除
      * @param id
      */
+    @Transactional
     public void delete(Integer id) {
-
-        //删除时，先将模板参数-1
-        Para para = paraMapper.selectByPrimaryKey(id);
-        Integer templateId = para.getTemplateId();
-        Template template = templateMapper.selectByPrimaryKey(templateId);
-        template.setParaNum(template.getParaNum() -1);
+        //将模板中的规格数量减一
+        Spec spec = specMapper.selectByPrimaryKey(id);
+        Template template = templateMapper.selectByPrimaryKey(spec.getTemplateId());
+        template.setSpecNum( template.getSpecNum()-1 );
         templateMapper.updateByPrimaryKey(template);
 
-        //执行删除操作
-        paraMapper.deleteByPrimaryKey(id);
+        specMapper.deleteByPrimaryKey(id);
     }
 
     /**
@@ -122,19 +122,19 @@ public class ParaServiceImpl implements ParaService {
      * @return
      */
     private Example createExample(Map<String, Object> searchMap){
-        Example example=new Example(Para.class);
+        Example example=new Example(Spec.class);
         Example.Criteria criteria = example.createCriteria();
         if(searchMap!=null){
             // 名称
             if(searchMap.get("name")!=null && !"".equals(searchMap.get("name"))){
                 criteria.andLike("name","%"+searchMap.get("name")+"%");
             }
-            // 选项
+            // 规格选项
             if(searchMap.get("options")!=null && !"".equals(searchMap.get("options"))){
                 criteria.andLike("options","%"+searchMap.get("options")+"%");
             }
 
-            // id
+            // ID
             if(searchMap.get("id")!=null ){
                 criteria.andEqualTo("id",searchMap.get("id"));
             }
