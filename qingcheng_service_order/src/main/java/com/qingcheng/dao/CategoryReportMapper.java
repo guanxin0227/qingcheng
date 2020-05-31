5a6e1a305e82c1d009deee6b0a510ed848599fbe
guanxin <guanxin9326@163.com>
2020-05-31 19:28:53 +0800

    1.定时任务2.按日期统计一级分类数据

--- qingcheng_service_order/src/main/java/com/qingcheng/dao/CategoryReportMapper.java
+++ qingcheng_service_order/src/main/java/com/qingcheng/dao/CategoryReportMapper.java
@@ -26,7 +26,7 @@ public interface CategoryReportMapper extends Mapper<CategoryReport> {
      * @return java.util.List<com.qingcheng.pojo.order.CategoryReport>
      **/
     @Select("select oi.category_id1 categoryId1,oi.category_id2 categoryId2,oi.category_id3 categoryId3,date_format(o.pay_time,'%Y-%m-%d') countDate,sum(oi.num) num,sum(oi.pay_money) money " +
-            "from qingcheng_order.tb_order_item oi, qingcheng_order.tb_order o " +
+            "from tb_order_item oi,tb_order o " +
             "where oi.order_id = o.id and o.pay_status ='1' and o.is_delete = '0' and date_format(o.pay_time,'%Y-%m-%d') = #{date} " +
             "group by oi.category_id1,oi.category_id2,oi.category_id3,date_format(o.pay_time,'%Y-%m-%d')")
     List<CategoryReport> categoryReport(@Param("date") LocalDate date);
@@ -38,9 +38,9 @@ public interface CategoryReportMapper extends Mapper<CategoryReport> {
      * @Param [startDate, endDate]
      * @return java.util.List<java.util.Map>
      **/
-    @Select("select category_id1 categoryId1, sum(num) num,sum(money) money " +
-            "from qingcheng_order.tb_category_report tcr " +
-            "where tcr.count_date >= #{startDate} and tcr.count_date <= #{endDate} " +
-            "group by tcr.category_id1")
+    @Select("select tcr.category_id1 categoryId1,vc.name categoryName, sum(num) num,sum(money) money " +
+            "from tb_category_report tcr,v_category1 vc " +
+            "where tcr.category_id1 = vc.id and tcr.count_date >= #{startDate} and tcr.count_date <= #{endDate} " +
+            "group by tcr.category_id1,vc.name")
     List<Map> category1Count(@Param("startDate") String startDate,@Param("endDate") String endDate);
 }
